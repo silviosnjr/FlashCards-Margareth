b9b7d9a13db38f5a12f119f1ec89070e72e47c1c
silviosnjr <silviosnjr@gmail.com>
2024-10-25 10:38:40 -0300

    first commit

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Química', 
+    'O que é um átomo?', 
+    'Um átomo é a menor unidade de um elemento químico'
+)
+
+criaCartao(
+    'Biologia', 
+    'Qual é a função do coração?', 
+    'O coração bombeia sangue para todo o corpo'
+)
+
+criaCartao(
+    'Geometria', 
+    'O que é um triângulo?', 
+    'Um triângulo é um polígono com três lados'
+)
+
+criaCartao(
+    'História', 
+    'Quem foi Cleópatra?', 
+    'Cleópatra foi a última faraó do Egito Antigo'
+)
+
+criaCartao(
+    'Astronomia', 
+    'O que é o sistema solar?', 
+    'O sistema solar é composto pelo Sol e todos os corpos que orbitam ao seu redor'
+)
+
+criaCartao(
+    'Português', 
+    'O que é um verbo?', 
+    'Verbo é uma palavra que indica ação, estado ou fenômeno'
+)
+
+criaCartao(
+    'Programação', 
+    'O que é uma variável?', 
+    'Uma variável é um espaço na memória que armazena valores que podem mudar'
+)
+
+criaCartao(
+    'Educação Física', 
+    'Qual é o benefício do alongamento?', 
+    'O alongamento ajuda a melhorar a flexibilidade e prevenir lesões'
+)
